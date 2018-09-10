da8c44214d38dc776e43c99b87f360291fe6b24f
Peter Leung <ys.leung.uk@gmail.com>
2018-09-10 14:08:34 +0800

    Initial Commit

---
+++ WhatsAppOnPad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatsAppOnPad
+//
+//  Created by Peter Leung on 10/9/2018.
+//  Copyright © 2018 Peter Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
